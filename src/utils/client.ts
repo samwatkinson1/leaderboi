1bd42ddb33fb3a153617ebd1ce598fff1c676757
Sam Watkinson <samwatkinson@outlook.com>
2022-07-09 17:38:08 +0100

    feat(utils/client): add bot invite link to ready response

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,5 +1,6 @@
 import { Client, Intents } from 'discord.js'
 import logger from './logger'
+import env from '../env'
 
 const client = new Client({
     intents: [
@@ -10,7 +11,14 @@ const client = new Client({
 })
 
 client.once('ready', c => {
+    const invite =
+        'https://discord.com/api/oauth2/authorize?client_id={client_id}&permissions=68608&scope=applications.commands%20bot'
+
     logger.info('bot', `Ready! Logged in as ${c.user.tag}`)
+    logger.info(
+        'bot',
+        `Use ${invite.replace('{client_id}', env.CLIENT_ID)} to invite bot to a server.`
+    )
 })
 
 export default client
