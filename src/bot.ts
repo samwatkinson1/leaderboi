e1e365a257e811643330e34700ae5401b94bf538
Sam Watkinson <samwatkinson@outlook.com>
2022-07-09 23:41:24 +0100

    feat(bot): wip: index all channel messages ever in guild

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,85 @@
 import client from './utils/client'
 import commandMap from './utils/commands'
+import logger from './utils/logger'
+import * as reactions from './controllers/message_reactions'
+import { indexAllMessageReactions } from './utils/message'
+
+let isIndexing = false
+
+const handleDbErr = (err: Error) => {
+    logger.error('db', err)
+}
+
+client.on('guildCreate', async guild => {
+    logger.info('bot', `Added to guild ${guild.name}`)
+
+    // TODO: Do we need to prevent other indexes?
+
+    isIndexing = true
+    client.user?.setPresence({ activities: [{ name: 'Indexing...' }] })
+    await indexAllMessageReactions(guild)
+    client.user?.setPresence({})
+    isIndexing = false
+})
+
+client.on('guildDelete', guild => {
+    logger.info('bot', `Removed from guild ${guild.name}`)
+    reactions
+        .destroy({ guild_id: guild.id })
+        .then(count => {
+            logger.info('db', `Deleted ${count} entry(s) for guild ${guild.id}`)
+        })
+        .catch(handleDbErr)
+})
+
+client.on('messageReactionAdd', async (reaction, user) => {
+    const newReaction = {
+        guild_id: reaction.message.guild?.id ?? '',
+        message_id: reaction.message.id,
+        reaction_id: reaction.emoji.identifier,
+        user_id: user.id
+    }
+
+    reactions
+        .create(newReaction)
+        .then(entry => {
+            logger.info('db', `User ${entry?.user_id} added reaction ${entry?.reaction_id}`)
+        })
+        .catch(handleDbErr)
+})
+
+client.on('messageReactionRemove', (reaction, user) => {
+    const removedReaction = {
+        message_id: reaction.message.id,
+        reaction_id: reaction.emoji.identifier,
+        user_id: user.id
+    }
+
+    reactions
+        .destroy(removedReaction)
+        .then(count => {
+            logger.info(
+                'db',
+                `Deleted ${count} entry(s) for message ${removedReaction.message_id}, reaction ${removedReaction.reaction_id}, user ${removedReaction.user_id}`
+            )
+        })
+        .catch(handleDbErr)
+})
+
+client.on('messageDelete', message => {
+    reactions
+        .destroy({ message_id: message.id })
+        .then(count => {
+            logger.info('db', `Deleted ${count} entry(s) for message ${message.id}`)
+        })
+        .catch(handleDbErr)
+})
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return
+
+    if (isIndexing)
+        return interaction.reply({ ephemeral: true, content: ':watch: Indexing please wait...' })
 
     const command = commandMap.get(interaction.commandName)
     if (command?.handler) command.handler(interaction)
---
+++ src/utils/message.ts
@@ -0,0 +1,50 @@
+import { Guild, MessageReaction, TextChannel } from 'discord.js'
+import logger from './logger'
+import * as messageReactions from '../controllers/message_reactions'
+
+const indexMessageReaction = async (reaction: MessageReaction) => {
+    logger.info('bot', `Indexing ${reaction.message.id}...`)
+    const users = await reaction.users.fetch()
+
+    await Promise.all(
+        users.map(user =>
+            messageReactions.create({
+                guild_id: reaction.message.guild?.id ?? '',
+                message_id: reaction.message.id,
+                reaction_id: reaction.emoji.identifier,
+                user_id: user.id
+            })
+        )
+    )
+
+    logger.info('bot', `Done ${reaction.message.id}`)
+}
+
+const indexMessageReactionsForChannel = async (channel: TextChannel) => {
+    logger.info('bot', `Indexing ${channel.name}...`)
+
+    const _f = async (before?: string): Promise<void> => {
+        const _m = await channel.messages.fetch({ limit: 100, before })
+        if (!_m.size) return
+
+        const reactions = _m.map(message => [...message.reactions.cache.values()]).flat()
+        await Promise.all(
+            reactions.filter(item => item.message.type === 'DEFAULT').map(indexMessageReaction)
+        )
+
+        return _f(_m.last()?.id)
+    }
+
+    return _f()
+}
+
+export const indexAllMessageReactions = async (guild: Guild) => {
+    const channels = await guild.channels.fetch(undefined, { cache: false })
+    const textChannels = [...channels.values()].filter(item => item.type === 'GUILD_TEXT')
+
+    return Promise.all(
+        textChannels.map(item => indexMessageReactionsForChannel(item as TextChannel))
+    )
+        .then(fulfilled => logger.info('bot', `Successfully indexed ${fulfilled.length} channels`))
+        .catch(rejected => logger.error('bot', `Failed to index ${rejected.length} channels`))
+}
