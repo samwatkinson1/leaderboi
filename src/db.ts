5dbd1c6e19687e4fed744cee40446bde3afa355b
Sam Watkinson <samwatkinson@outlook.com>
2022-07-09 23:39:13 +0100

    fix(db): use default connection db and log

--- src/db.ts
+++ src/db.ts
@@ -5,8 +5,7 @@ import logger from './utils/logger'
 const start = async () => {
     try {
         const instance = await mongoose.connect(env.MONGO_URL)
-        instance.connection?.useDb(env.MONGO_DATABASE)
-        logger.info('db', `Connected to ${env.MONGO_URL} using ${env.MONGO_DATABASE}`)
+        logger.info('db', `Connected to ${env.MONGO_URL} using ${instance.connection?.name}`)
     } catch (e) {
         logger.error('db', `Could not connect to ${env.MONGO_URL}`, e)
     }
