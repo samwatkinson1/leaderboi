8eec17bc896664bf2e5837746fd945f0105e6ea7
Sam Watkinson <samwatkinson@outlook.com>
2022-07-09 17:31:51 +0100

    fix: revert changes around /api/bot/interactions

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,12 +1,9 @@
 import { Router } from 'express'
-import botRouter from './bot'
 
 const route = Router()
 
 route.get('/health', (req, res) => {
     res.sendStatus(200)
 })
-
-route.use('/bot', botRouter)
 
 export default route
